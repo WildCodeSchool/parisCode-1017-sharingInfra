dfd47f25ab49726657ca529ce3a6497d22217059
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2018-01-10 16:47:42 +0100

    navbar integration and style

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ Encore
 
     // will create web/build/app.js and web/build/app.css
     .addEntry('main', path.join(__dirname, 'web', 'assets', 'js', 'main.js'))
+    .addEntry('style', path.join(__dirname, 'web', 'assets', 'css', 'style.scss'))
 
     // allow sass/scss files to be processed
     .enableSassLoader()
