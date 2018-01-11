9c64734ef8f7e55cba1cda37542831c261e0d970
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2018-01-11 11:30:19 +0100

    search field homepage implementation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\AdvertType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +14,15 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $form = $this->createForm(AdvertType::class, null, [
+            'method' => 'GET',
+            'action' => $this->get('router')->generate('resultspage')
+        ]);
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createView()
         ]);
     }
 
