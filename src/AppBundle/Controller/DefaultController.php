aa7911cd80641e0d2a6552cb74384cbea89db786
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2018-01-13 17:10:49 +0100

    contact form on faq page is woooorking

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Form\AdvertType;
+use AppBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,9 +32,34 @@ class DefaultController extends Controller
      *
      * @Route("/faq", name="faq")
      */
-    public function faqAction()
+    public function faqAction(Request $request)
     {
-        return $this->render('default/faq.html.twig');
+        $form = $this->createForm(ContactType::class,null,array(
+            'action' => $this->generateUrl('faq'),
+            'method' => 'POST'
+        ));
+
+        if ($request->isMethod('POST')) {
+            // Refill the fields in case the form is not valid.
+            $form->handleRequest($request);
+
+            if($form->isValid()){
+                // Send mail
+                if($this->sendEmail($form->getData())){
+
+                    // Everything OK, redirect to wherever you want ! :
+
+                    return $this->redirectToRoute('faq');
+                }else{
+                    // An error ocurred, handle
+                    var_dump("Errooooor :(");
+                }
+            }
+        }
+
+        return $this->render('default/faq.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
     /**
@@ -43,6 +69,29 @@ class DefaultController extends Controller
      */
     public function legalMentionsAction()
     {
+        return $this->render('default/legal_mentions.html.twig');
+    }
+
+    /**
+     * @param $data
+     * @return int|\Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/mail", name="mail")
+     */
+    public function sendEmail()
+    {
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Demande d\'information')
+            ->setFrom($this->getParameter('mailer_user'))
+            ->setTo('teamhobbea@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    'default/mail_template.html.twig'),
+                'text/html'
+            );
+
+        $this->get('mailer')->send($message);
+
         return $this->render('default/legal_mentions.html.twig');
     }
 
---
+++ src/AppBundle/Form/ContactType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array('attr' => array('placeholder' => 'Votre nom'),
+                'constraints' => array(
+                    new NotBlank(array("message" => "Merci de renseigner votre nom.")),
+                )
+            ))
+            ->add('email', EmailType::class, array('attr' => array('placeholder' => 'Votre email'),
+                'constraints' => array(
+                    new NotBlank(array("message" => "Merci de renseigner votre email.")),
+                    new Email(array("message" => "Le format de votre email n'est pas valide.")),
+                )
+            ))
+            ->add('message', TextareaType::class, array('attr' => array('placeholder' => 'Votre message'),
+                'constraints' => array(
+                    new NotBlank(array("message" => "Merci de renseigner un message.")),
+                )
+            ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'error_bubbling' => true
+        ));
+    }
+
+    public function getName()
+    {
+        return 'contact_form';
+    }
+}
