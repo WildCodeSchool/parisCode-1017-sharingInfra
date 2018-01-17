d292484725d8e0a211a813e20ad7b2640696ca3e
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2018-01-17 16:10:31 +0100

    style on contact form on faq page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,7 +20,6 @@ class DefaultController extends Controller
             'action' => $this->get('router')->generate('resultspage')
         ]);
 
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
             'form' => $form->createView()
--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -3,23 +3,33 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
+
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
+        $builder
+            ->remove('username')
+            ->add('firstname')
+            ->add('name')
+            ->add('email')
+        ;
     }
 
     public function getParent()
     {
-        return 'fos_user_registration';
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
     }
 
     public function getName()
     {
-        return 'app_user_registration';
+        return $this->getBlockPrefix();
     }
 }
