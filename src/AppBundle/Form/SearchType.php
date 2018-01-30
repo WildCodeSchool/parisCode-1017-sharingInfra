87ca5787714bc2022ac7e9de7314220bdfe42b20
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2018-01-30 09:51:44 +0100

    search bar with checkbox is working

--- src/AppBundle/Form/SearchType.php
+++ src/AppBundle/Form/SearchType.php
@@ -2,7 +2,11 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,12 +23,17 @@ class SearchType extends AbstractType
             'label'=> false,
             'attr' => ['class'=>'form-control input-lg', 'placeholder'=> 'Votre destination'],
             'required' => false,
-        ));
-        $builder->add('type', TextType::CLASS, [
+        ))
+            ->add('type', EntityType::class, array(
+            'class' => Type::class,
+            'choice_label' => 'gearType',
+            'multiple' => true,
+            'expanded' => true,
             'label' => false,
-            'attr' => ['class'=>'form-control input-lg', 'placeholder'=> 'Votre type'],
-            'required' => false,
-        ]);
+            'required' =>false
+        ));
+            // 'attr' => ['class'=>'form-control input-lg', 'placeholder'=> 'Votre type'],
+            // 'required' => false,
     }
 
     /**
